f4bfd6a4cf968c44cf68a64f0f3e5c9db7f8e4cf
Eduardo Rosa <92902577+RosaMaster@users.noreply.github.com>
2023-07-19 23:16:10 -0300

    Insert new exercise

---
+++ exercicios_level_05/src/application/exercicio07.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import util.Calculator;
+
+public class exercicio07 {
+
+	public static void main(String[] args) {
+		/* METODO 02 : Fazer um programa para ler um valor numérico qualquer, e daí mostrar 
+		quanto seria o valor de uma circunferência e do volume de uma esfera 
+		para um raio daquele valor. Informar também o valor de PI com duas 
+		casas decimais */
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Calculator calc = new Calculator();
+		
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = calc.circumference(radius);
+		
+		double v = calc.volume(radius);
+		
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", calc.PI);
+		
+		
+		sc.close();
+	}
+
+}
